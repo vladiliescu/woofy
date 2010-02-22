53121bf9dbd63f0783de11c43a8f87c818af9cec
vlad.iliescu <devnull@localhost>
2010-02-22 14:18:48 +0200

    added a mind-map for keeping track of all the stuff.

---
+++ todo.mm
@@ -0,0 +1,29 @@
+<map version="0.9.0">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1266481859626" ID="ID_1013247563" MODIFIED="1266481865986" TEXT="Woofy">
+<node CREATED="1266481866595" FOLDED="true" ID="ID_319954357" MODIFIED="1266840998892" POSITION="right" TEXT="miscellaneous">
+<node CREATED="1266481875174" ID="ID_1542976632" MODIFIED="1266481890831" TEXT="old definitions">
+<node CREATED="1266481891347" ID="ID_115237448" MODIFIED="1266481926490" TEXT="upload a zip file containing the old definitions + readme describing how to install them"/>
+<node CREATED="1266481901035" ID="ID_598591838" MODIFIED="1266481918333" TEXT="warn about the definitions not being supported anymore"/>
+</node>
+</node>
+<node CREATED="1266481938413" FOLDED="true" ID="ID_1917410620" MODIFIED="1266840999704" POSITION="left" TEXT="marketing">
+<node CREATED="1266481944476" ID="ID_1016717826" MODIFIED="1266481950648" TEXT="reupload to upload.com"/>
+<node CREATED="1266481951851" ID="ID_102725260" MODIFIED="1266481956039" TEXT="reupload to softpedia"/>
+<node CREATED="1266827664243" ID="ID_1117038780" MODIFIED="1266827687852" TEXT="update the PAD file homepage (see what else I can update there)"/>
+</node>
+<node CREATED="1266482121535" ID="ID_908633401" MODIFIED="1266482124535" POSITION="right" TEXT="architecture"/>
+<node CREATED="1266482126098" ID="ID_1431455119" MODIFIED="1266482130536" POSITION="left" TEXT="small fixes"/>
+<node CREATED="1266840597508" ID="ID_1420662572" MODIFIED="1266841008861" POSITION="right" TEXT="release - checklist">
+<node CREATED="1266840712605" ID="ID_1585378337" MODIFIED="1266840724199" TEXT="compile a whatsnew list"/>
+<node CREATED="1266840616806" ID="ID_1813123511" MODIFIED="1266840667526" TEXT="build Woofy using the local repository (or the primary one, but don&apos;t forget to update it)"/>
+<node CREATED="1266840670260" ID="ID_116153230" MODIFIED="1266840681432" TEXT="upload the setup and zip file to Google Code"/>
+<node CREATED="1266840683620" ID="ID_1426800359" MODIFIED="1266840693058" TEXT="update the updatesDescriptionFile.xml">
+<node CREATED="1266840693402" ID="ID_1332316660" MODIFIED="1266840696167" TEXT="on Google Code"/>
+<node CREATED="1266840696745" ID="ID_1934389367" MODIFIED="1266840702855" TEXT="on SourceForge"/>
+</node>
+<node CREATED="1266840727652" ID="ID_1932120632" MODIFIED="1266840758200" TEXT="update the WhatsNew wiki page with the new content"/>
+<node CREATED="1266840739918" ID="ID_522348351" MODIFIED="1266840754403" TEXT="update the Comics wiki page"/>
+</node>
+</node>
+</map>
