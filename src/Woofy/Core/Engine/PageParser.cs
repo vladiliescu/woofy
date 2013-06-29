aa97520443ae81a7e3374a1e012f12441139322d
Vlad Iliescu <vlad.iliescu@gmail.com>
2013-06-29 23:24:15 +0300

    We finally support the <base> HTML element. See http://www.w3.org/TR/html5/document-metadata.html#the-base-element for more details.

--- src/Woofy/Core/Engine/PageParser.cs
+++ src/Woofy/Core/Engine/PageParser.cs
@@ -23,14 +23,21 @@ namespace Woofy.Core.Engine
         public Uri[] RetrieveLinksFromPage(string regex, Uri currentUri, string pageContent, Action<string, string> reportFormatException)
 		{
 			var rawLinks = RetrieveContent(regex, pageContent);
+            if (rawLinks.Length == 0)
+                return new Uri[0];
+
+            var baseUri = GetBaseUri(pageContent);
 			var links = new List<Uri>();
 			foreach (var link in rawLinks)
 			{
                 try
                 {
-                    links.Add(Uri.IsWellFormedUriString(link, UriKind.Absolute)
-                        ? new Uri(link)
-                        : new Uri(currentUri, link));
+                    if (Uri.IsWellFormedUriString(link, UriKind.Absolute))
+                        links.Add(new Uri(link));
+                    else if (baseUri != null)
+                        links.Add(new Uri(baseUri, link));
+                    else
+                        links.Add(new Uri(currentUri, link));
                 }
                 catch (UriFormatException)
                 {
@@ -41,7 +48,20 @@ namespace Woofy.Core.Engine
 			return links.ToArray();
 		}
 
-    	public string[] RetrieveContent(string regex, string pageContent)
+        private Uri GetBaseUri(string pageContent)
+        {
+            var baseElement = RetrieveContent(@"<base(.*?)href=""(?<content>.*?)""", pageContent);
+            if (baseElement.Length == 0)
+                return null;
+
+            Uri baseUri;
+            if (!Uri.TryCreate(baseElement[0], UriKind.Absolute, out baseUri))
+                return null;
+
+            return baseUri;
+        }
+
+        public string[] RetrieveContent(string regex, string pageContent)
     	{
 			var matches = Regex.Matches(pageContent, regex, RegexOptions.IgnoreCase | RegexOptions.Multiline);
 
