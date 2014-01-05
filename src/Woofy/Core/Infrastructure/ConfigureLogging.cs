bdb97e62c4d01243d9b6b0ae3aa862eaba057f5d
Vlad Iliescu <vlad.iliescu@gmail.com>
2014-01-05 20:54:59 +0200

    Updated NLog to the latest version, errors are now logged in a flat file instead of the Event Log.

--- src/Woofy/Core/Infrastructure/ConfigureLogging.cs
+++ src/Woofy/Core/Infrastructure/ConfigureLogging.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using NLog;
 using NLog.Config;
 using NLog.Targets;
@@ -12,19 +13,19 @@ namespace Woofy.Core.Infrastructure
             
             AddErrorRule(config);
 
-            LogManager.Configuration = config;
+            LogManager.Configuration = config;  
         }
 
         private static void AddErrorRule(LoggingConfiguration config)
         {
-            var appInfo = ContainerAccessor.Resolve<IAppInfo>();
+            var errorTarget = new FileTarget
+            {
+                Layout = "${date} ${message} ${exception:format=ToString}",
+                FileName = "${basedir}/error.txt",
+                Encoding = Encoding.UTF8,
+                KeepFileOpen = false,
+            };
 
-            var errorTarget = new EventLogTarget
-                                  {
-                                      Log = "Application",
-                                      Source = appInfo.NameAndVersion,
-                                      Layout = "${date} ${message} ${exception:format=ToString}"
-                                  };
             var errorRule = new LoggingRule("*", LogLevel.Error, errorTarget);
             config.AddTarget("error", errorTarget);
             config.LoggingRules.Add(errorRule);
